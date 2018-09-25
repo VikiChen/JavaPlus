7b19d007961b99a0dc968600f637b8109d6d8e79
Viki Chen <viki@chinadep.com>
2018-09-25 10:03:47 +0800

    1.空壳子

---
+++ src/main/java/com/viki/javaplus/JavaplusApplication.java
@@ -0,0 +1,12 @@
+package com.viki.javaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaplusApplication.class, args);
+    }
+}
